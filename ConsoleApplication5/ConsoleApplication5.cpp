b2aa7fbfce1f3c2dca4f11e2a28ee6f6886944e3
KopoHaMuHyc <KopoHaMuHyc@DESKTOP-E0EH69H>
2024-03-13 17:50:27 +0200

    Add project files.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream> 
+using namespace std;
+
+void Rectangle(double height, double width, double* square, double* perimeter)
+{
+    *square = width * height;
+    *perimeter = 2 * (width + height);
+}
+
+int main()
+{
+    double square = 0;
+    double perimeter = 0;
+    double width;
+    cout << "Input width: ";
+    cin >> width;
+    double height;
+    cout << "Input height: ";
+    cin >> height;
+    Rectangle(width, height, &square, &perimeter);
+    cout << "Square: " << square << ", Perimeter: " << perimeter << "\n";
+
+    return 0; 
